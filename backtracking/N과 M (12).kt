802ab467a129989ccff0f7a65e0de3748d22ac79
훈성 <phunseong@gmail.com>
2021-12-18 02:32:33 +0900

    백준[15666] N과 M (12)

---
+++ backtracking/N과 M (12).kt
@@ -0,0 +1,42 @@
+package com.hunseong.al_prac
+
+import java.util.*
+
+var N = 0
+var M = 0
+var nums: Array<Int>? = null
+var selected: Array<Int>? = null
+var sb = StringBuilder()
+fun main() = with(Scanner(System.`in`)) {
+    N = nextInt()
+    M = nextInt()
+    nums = Array(N + 1) { 0 }
+    selected = Array(M + 1) { 0 }
+    for (i in 1..N) {
+        nums!![i] = nextInt()
+    }
+
+    nums!!.sort()
+
+    dfs(1)
+    println(sb.toString())
+
+}
+
+fun dfs(k: Int) {
+    if (k > M) {
+        for (i in 1..M) {
+            sb.append(selected!![i]).append(' ')
+        }
+        sb.append("\n")
+        return
+    }
+
+    var prev = -1
+    for (i in 1..N) {
+        if (prev == nums!![i] || selected!![k - 1] > nums!![i]) continue
+        selected!![k] = nums!![i]
+        prev = nums!![i]
+        dfs(k + 1)
+    }
+}
