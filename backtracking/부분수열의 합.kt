d371aa4f9c46eca9fd9da60921138ab7b7bbcdd6
훈성 <phunseong@gmail.com>
2022-01-02 23:33:46 +0900

    백준[1182] 부분수열의 합

--- backtracking/부분수열의 합.kt
+++ backtracking/부분수열의 합.kt
@@ -1,33 +1,30 @@
-package com.hunseong.al_prac
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
-import java.util.*
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (N, S) = br.readLine().split(" ").map { it.toInt() }
+    val nums = arrayListOf<Int>()
+    br.readLine().split(" ").map { nums.add(it.toInt()) }
+    var count = 0
+    if (S == 0) count--
 
-var N = 0
-var S = 0
-var result = 0
-var nums: Array<Int>? = null
-fun main() = with(Scanner(System.`in`)){
-    N = nextInt()
-    S = nextInt()
-    nums = Array(N) { 0 }
-    for (i in 0 until N) {
-        nums!![i] = nextInt()
+    fun dfs(k: Int, value: Int) {
+        if (k == N) {
+            if (value == S) count++
+        } else {
+            dfs(k+1, value)
+            dfs(k+1, value + nums[k])
+        }
     }
-    // 진 부분수열이므로 S=0일 시 공집합의 결과는 미리 제외
-    if (S == 0) result --
 
     dfs(0, 0)
-    println(result)
-}
-
-fun dfs(index: Int, value: Int) {
-    if (index >= N) {
-        if (S == value) result++
-        return
-    }
-
-    // 각 nums[index]를 포함 / 포함하지 않은 상태로 재귀함수 호출
-    dfs(index + 1, value)
-    dfs(index + 1, value + nums!![index])
+    bw.write("$count")
+    bw.flush()
+    bw.close()
+    br.close()
 }
 
