e317f759c9a63a1ec6d9ed86658ab764e3b26cee
HunSeongPark <phunseong@gmail.com>
2022-12-19 19:08:12 +0900

    백준[10451] 순열 사이클 - BFS

---
+++ bfs/B_10451.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class B_10451 {
+
+    static ArrayList<Integer>[] A;
+    static PriorityQueue<Integer> queue = new PriorityQueue<>();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        int T;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            A = new ArrayList[N + 1];
+            visited = new boolean[N + 1];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                A[i] = new ArrayList<>();
+                A[i].add(Integer.parseInt(st.nextToken()));
+            }
+            int count = 0;
+            for (int i = 1; i <= N; i++) {
+                if (!visited[i]) {
+                    count++;
+                    bfs(i);
+                }
+            }
+            System.out.println(count);
+        }
+        br.close();
+    }
+
+    private static void bfs(int n) {
+        queue.add(n);
+        visited[n] = true;
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+            for (Integer i : A[poll]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
