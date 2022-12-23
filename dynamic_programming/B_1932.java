29b7a20b9da5afe2a22fe45aaf1a958dce2e0b33
HunSeongPark <phunseong@gmail.com>
2022-12-23 19:39:18 +0900

    백준[1932] 정수 삼각형

--- dynamic_programming/B_1932.java
+++ dynamic_programming/B_1932.java
@@ -1,54 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B_1932 {
 
     static int n;
-    static ArrayList<Integer>[] A;
+    static int[][] A;
     static int[][] DP;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        A = new ArrayList[n];
+        A = new int[n][n];
         DP = new int[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                DP[i][j] = -1;
-            }
+            Arrays.fill(DP[i], -1);
         }
         for (int i = 0; i < n; i++) {
-            A[i] = new ArrayList<>();
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j <= i; j++) {
                 int n = Integer.parseInt(st.nextToken());
-                A[i].add(n);
+                A[i][j] = n;
             }
         }
-        int max = 0;
-        DP[0][0] = A[0].get(0);
         for (int i = 0; i < n; i++) {
-            int sum = dp(n - 1, i);
-            if (sum > max) {
-                max = sum;
-            }
+            DP[n - 1][i] = A[n - 1][i];
         }
-        System.out.println(max);
+        System.out.println(dp(0, 0));
     }
 
     private static int dp(int n, int idx) {
         if (DP[n][idx] == -1) {
-            if (idx == 0) {
-                DP[n][idx] = dp(n - 1, idx) + A[n].get(idx);
-            } else if (n == idx) {
-                DP[n][idx] = dp(n - 1, idx - 1) + A[n].get(idx);
-            } else {
-                DP[n][idx] = Math.max(dp(n - 1, idx - 1), dp(n - 1, idx)) + A[n].get(idx);
-            }
+            DP[n][idx] = A[n][idx] + Math.max(dp(n + 1, idx), dp(n + 1, idx + 1));
         }
         return DP[n][idx];
     }
