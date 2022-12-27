e1fdd3a635a3f979d6ff88188c11ba7be0b98e1a
HunSeongPark <phunseong@gmail.com>
2022-12-27 16:49:22 +0900

    백준[9655] 돌 게임

---
+++ math/B_9655.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_9655 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N % 2 == 0) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+    }
+}
