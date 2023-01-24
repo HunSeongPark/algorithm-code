6a7fd0878be6d2f0c1dca78bd3387d95cdc9e0fd
HunSeongPark <phunseong@gmail.com>
2023-01-24 17:58:07 +0900

    백준[2437] 저울

---
+++ greedy/B_2437.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_2437 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        for (int i = 0; i < N; i++) {
+            if (sum + 1 < A[i]) {
+                break;
+            }
+            sum += A[i];
+        }
+        System.out.println(sum + 1);
+    }
+}
