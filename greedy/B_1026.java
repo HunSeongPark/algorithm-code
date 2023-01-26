5a71eae380803cff0462b1af8dec8f4506d92399
HunSeongPark <phunseong@gmail.com>
2023-01-26 23:39:51 +0900

    백준[1026] 보물

---
+++ greedy/B_1026.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_1026 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] B = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            result += A[i] * B[N - 1 - i];
+        }
+        System.out.println(result);
+    }
+}
