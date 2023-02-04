c42c857c98affa68f78c2b56be44cc16ee406c77
HunSeongPark <phunseong@gmail.com>
2023-02-04 14:22:26 +0900

    백준[18310] 안테나

---
+++ greedy/B_18310.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_18310 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        System.out.println(A[(N - 1) / 2]);
+    }
+}
