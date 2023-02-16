6f0ae8fd38ab6e4ff3a137c50897db6aef8611e5
HunSeongPark <phunseong@gmail.com>
2023-02-16 21:35:07 +0900

    백준[11048] 이동하기

---
+++ dynamic-programming/B_11048.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_11048 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                map[i][j] += Math.max(map[i][j - 1], Math.max(map[i - 1][j], map[i - 1][j - 1]));
+            }
+        }
+        System.out.println(map[N][M]);
+    }
+}
