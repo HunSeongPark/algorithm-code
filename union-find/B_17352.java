ecc04780f482165dff84773b0c1dc10b5a970afe
HunSeongPark <phunseong@gmail.com>
2024-03-14 18:51:43 +0900

    백준[17352] 여러분의 다리가 되어드리겠습니다!

---
+++ union-find/B_17352.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_17352 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+}
